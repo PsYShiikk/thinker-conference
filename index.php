ad5863edb47e6787e016ef86e7975f01ecfdb2c1
Léo Delplanque <delplanqueleomail@gmail.com>
2019-12-21 14:05:31 +0100

    first commit to push start

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <!-- Icone du site -->
+  <link rel="shortcut icon" href="assets/img/logo/icon_thinker.png" type="image/x-icon">
+  <link rel="icon" href="assets/img/logo/icon_thinker.png" type="image/x-icon">
+
+  <!-- Chargement des styles -->
+  <link rel="stylesheet" href="assets/css/normalize.css">
+  <link rel="stylesheet" href="assets/css/master.css">
+
+  <!-- Chargement des fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+
+  <!-- Titre de la page -->
+  <title>Thinker Conférence</title>
+</head>
+<body>
+
+
+<div class="all">
+  <h1 class="titre">Le site est en construction.</h1>
+  <div class="logo"></div>
+</div>
+
+</body>
+</html>
